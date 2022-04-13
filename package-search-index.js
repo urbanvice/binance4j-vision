a7c79eb9048849bca7e4e40b8859c8510925819b
vivienld <68043710+vivienld@users.noreply.github.com>
2022-04-13 00:56:22 +0000

    Deploying to gh-pages from @ binance4j/binance4j-vision@a34970550a5edc2615ed7e93c1c640eff81fd50a 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.binance4j.vision.executor"},{"l":"com.binance4j.vision.spot"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.binance4j.vision.enums"},{"l":"com.binance4j.vision.executor"},{"l":"com.binance4j.vision.spot"}];updateSearchResults();
